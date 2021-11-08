2d4b15c285797614a6a6809bea7a87caf4488c9f
shin.kumagai <shin.kumagai.216@gmail.com>
2021-11-08 09:30:49 +0000

    lesson5

---
+++ kadai-introduce.rb
@@ -0,0 +1,11 @@
+#パターン１
+myouzi = '田中'
+namae = '太郎'
+nenrei = '25'
+
+puts myouzi + namae + 'です。' + nenrei + '歳です'
+
+#パターン２
+#profile = [myouzi = '田中', namae = '太郎', nenrei = '25']
+
+#puts profile[0] + profile[1] + 'です。' + profile[2] + '歳です。'
