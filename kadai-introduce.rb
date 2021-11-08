cff036c48500338ea502e5b3c65df2bc1441dd34
shin.kumagai <shin.kumagai.216@gmail.com>
2021-11-08 12:56:23 +0000

    kadai

--- kadai-introduce.rb
+++ kadai-introduce.rb
@@ -3,9 +3,9 @@ myouzi = '田中'
 namae = '太郎'
 nenrei = 25
 
-puts myouzi + namae + 'です。' + nenrei + '歳です'
+puts myouzi + namae + 'です。' + nenrei.to_s + '歳です'
 
 #パターン２
-#profile = [myouzi = '田中', namae = '太郎', nenrei = '25']
+#profile = [myouzi = '田中', namae = '太郎', nenrei = 25]
 
-#puts profile[0] + profile[1] + 'です。' + profile[2] + '歳です。'
+#puts profile[0] + profile[1] + 'です。' + profile[2].to_s + '歳です。'
