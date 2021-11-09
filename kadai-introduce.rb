9e2bca2c3c229ce4007b8da492ac3cad2e19e736
shin.kumagai <shin.kumagai.216@gmail.com>
2021-11-09 17:52:40 +0000

    add symbol

--- kadai-introduce.rb
+++ kadai-introduce.rb
@@ -3,9 +3,16 @@ myouzi = '田中'
 namae = '太郎'
 nenrei = 25
 
-puts myouzi + namae + 'です。' + nenrei.to_s + '歳です'
+puts myouzi + namae + 'です。' + nenrei.to_s + '歳です。'
+
 
 #パターン２
 #profile = [myouzi = '田中', namae = '太郎', nenrei = 25]
 
 #puts profile[0] + profile[1] + 'です。' + profile[2].to_s + '歳です。'
+
+
+#パターン３
+#profile = {myouzi: "田中", namae: "太郎", nenrei: 25}
+
+#puts profile[:myouzi] + profile[:namae] + 'です。' + profile[:nenrei].to_s + '歳です。'
